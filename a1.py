1434fa58a9e07869dcae8d232db9949e94a351ba
Rachel Tucker <rach1234@live.unc.edu>
2020-09-16 15:48:33 -0400

    A1

--- a1.py
+++ a1.py
@@ -2,19 +2,20 @@ import sys
 #print(sys.argv[0]) #the command
 #print(sys.argv[1]) #the parameter to the command (what you will be working with)
 
-
+my_dict_unsort = {}
 my_set = open(sys.argv[1]).read().split("\n") #transforms inputted text file into a list
+for var in my_set: #turns the list of strings into a list of integers and puts them in a dictionary where the keys are the number of times the component is repeated
+    hex = int(var, 16) #base 16 hex
+    if not hex in my_dict_unsort:
+        my_dict_unsort[hex] = 1
+    else:
+        my_dict_unsort[hex] += 1
 #print(my_set)
-my_dict = {i: my_set.count(i) for i in my_set} #creates a dictionary where the keys are then number of times a component is repeated
 #print(my_dict)
-for x in my_dict:
-    if x[0] == '0': #takes care of the cases of a number starting with 0
-        if len(x) > 1:
-            copy = x.replace('0', '')
-        if copy[0] == 0: #if my copy is now empty because the value was all zeros
-            copy = '0'
-            print copy, my_dict[x];
-        else: #if my copy isn't empty after deleting zeroes
-            print copy, my_dict[x];
-    else: #if my string does not begin with a zero
-        print x,my_dict[x]
+vars = my_dict_unsort.keys()
+vars.sort(); #sorts all my int variables
+
+for x in vars: #iterates through my sorted keys and prints their string value and the number of times they repeat
+    if my_dict_unsort[x] >= 2:
+        #print x,my_dict_unsort[x]
+        print "%x" % x + " " + str(my_dict_unsort[x])
